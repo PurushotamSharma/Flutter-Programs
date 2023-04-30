a853479877f647b44010f6f28c3f89ce0dcd780d
Purushotam Sharma <107574991+PurushotamSharma@users.noreply.github.com>
2023-04-30 16:52:30 +0530

    Update program4.dart

--- lib/program4.dart
+++ lib/program4.dart
@@ -1,17 +1,24 @@
-
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
-class FirstScreen extends StatefulWidget {
-  @override
-  _FirstScreenState createState() => _FirstScreenState();
+void main() {
+  runApp(MaterialApp(
+    home: const Program_4(),
+  ));
 }
 
-class _FirstScreenState extends State<FirstScreen> {
+class Program_4 extends StatefulWidget {
+  const Program_4({Key? key}) : super(key: key);
+
+  @override
+  State<Program_4> createState() => _Program_4State();
+}
+
+class _Program_4State extends State<Program_4> {
+  final _formKey = GlobalKey<FormState>();
   final TextEditingController _firstNameController = TextEditingController();
   DateTime _selectedDate = DateTime.now();
 
-  Future<void> _selectDate(BuildContext context) async {
+  void _selectDate(BuildContext context) async {
     final DateTime? picked = await showDatePicker(
       context: context,
       initialDate: _selectedDate,
@@ -25,44 +32,92 @@ class _FirstScreenState extends State<FirstScreen> {
     }
   }
 
+  void _goToNextScreen() {
+    if (_formKey.currentState!.validate()) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => new NextScreen(
+            firstName: _firstNameController.text,
+            selectedDate: _selectedDate,
+          ),
+        ),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('First Screen'),
+        title: Text("Program 4"),
+      ),
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+                controller: _firstNameController,
+                decoration: InputDecoration(
+                  hintText: 'Enter your first name',
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your first name';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 16.0),
+              Column(
+                children: [
+                  Text('SelectedDate: ${_selectedDate.toString()}'),
+                  SizedBox(width: 16.0),
+                  ElevatedButton(
+                    onPressed: () => _selectDate(context),
+                    child: Text('Select Date'),
+                  ),
+                ],
+              ),
+              SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: _goToNextScreen,
+                child: Text('Go to next screen'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class NextScreen extends StatelessWidget {
+  final String firstName;
+  final DateTime selectedDate;
+
+  const NextScreen({
+    Key? key,
+    required this.firstName,
+    required this.selectedDate,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Next Screen'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            TextField(
-              controller: _firstNameController,
-              decoration: InputDecoration(
-                hintText: 'Enter your first name',
-              ),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () => _selectDate(context),
-              child: Text('Select Date'),
-            ),
-            SizedBox(height: 20),
-            Text(
-              'Selected Date: ${DateFormat.yMd().format(_selectedDate)}',
-              style: TextStyle(fontSize: 20),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(
-                  context,
-                  '/second',
-                  arguments:
-                  '${_firstNameController.text} - ${DateFormat.yMd().format(_selectedDate)}',
-                );
-              },
-              child: Text('Go to next screen'),
-            ),
+            Text('First Name: $firstName'),
+            SizedBox(height: 16.0),
+            Text('Selected Date: ${selectedDate.toString()}'),
           ],
         ),
       ),
