33660e83764a9ebf218cc25f02e747f6ce82f02e
Purushotam Sharma <107574991+PurushotamSharma@users.noreply.github.com>
2023-04-30 23:09:03 +0530

    Update program3.dart

--- lib/program3.dart
+++ lib/program3.dart
@@ -1,28 +1,43 @@
 import 'package:flutter/material.dart';
-void main(){
-  runApp(MyApp());
+void main() {
+  runApp( MaterialApp(
+    home:  Program_3() ,
+  ),);
 }
-class MyApp extends StatelessWidget{
+class Program_3 extends StatelessWidget {
+  const Program_3({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return
-      Row(
-        children: [
-          Expanded(child: Column(
-            children: [
-              Image.asset('assets.image1.jpg'),
-              Image.asset('assets.image2.jpg'),
-              Image.asset('assets.image3.jpg'),
-            ],
-          )),
-          // Expanded(child: Column(
-          //   children: [
-          //     Image.network("https://www.google.com/search?q=images&sxsrf=APwXEddVdn5WZtonQLSm30dvzNIBO3cWDg:1682605551492&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiDiMaRosr-AhU6SmwGHVaUDqoQ_AUoAXoECAIQAw&biw=1536&bih=750&dpr=1.25#imgrc=9SPhZ2nyEGps3M"),
-          //     Image.network("https://www.google.com/search?q=images&sxsrf=APwXEddVdn5WZtonQLSm30dvzNIBO3cWDg:1682605551492&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiDiMaRosr-AhU6SmwGHVaUDqoQ_AUoAXoECAIQAw&biw=1536&bih=750&dpr=1.25#imgrc=ISsdmwh92GPtrM"),
-          //   ],
-          // ))
-        ],
-      );
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Program 3"),
+        ),
+        body: Column(
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: Image.asset('assets/images/img1.jpg'),
+                ),
+                Expanded(
+                  child: Image.asset('assets/images/img2.jpg'),
+                ),
+                Expanded(
+                  child: Image.asset('assets/images/img3.jpg'),
+                ),
+              ],
+            ),
+            const SizedBox(height: 16),
+            Image.network(
+                'https://static.toiimg.com/thumb/msid-98290591,width-1280,resizemode-4/98290591.jpg'),
+            const SizedBox(height: 16),
+            Image.network(
+                'https://static.toiimg.com/thumb/msid-98290591,width-1280,resizemode-4/98290591.jpg'),
+          ],
+        ),
+      ),
+    );
   }
-
 }
